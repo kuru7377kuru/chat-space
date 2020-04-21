01ed8d92596fb3ab7ca65838163b28004444ae2b
kuru7377kuru <warukyuwarukyu@gmail.com>
2020-04-22 08:40:15 +0900

    commit_0422_02

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chat-space'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space'
+  config.fog_directory  = 'kuruguru7377'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kuruguru7377'
 end
